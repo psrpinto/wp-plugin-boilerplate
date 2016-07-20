31a2619a5a7ae0eb4b09ede597aa346580d61849
Paulo Rodrigues Pinto <regularjack@gmail.com>
2016-07-20 01:13:30 +0100

    Rename main plugin file to plugin.php

--- tests/lib/bootstrap.php
+++ tests/lib/bootstrap.php
@@ -5,7 +5,7 @@ require_once __DIR__.'/includes/functions.php';
 
 // Manually load the plugin being tested
 tests_add_filter('muplugins_loaded', function () {
-    require __DIR__.'/../../wp-plugin-boilerplate.php';
+    require __DIR__.'/../../plugin.php';
 });
 
 // Start up the WP testing environment
